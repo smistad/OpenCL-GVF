2e853a6edd8651c1da8ac2c558b47819c159797d
Erik Smistad <smistad@idi.ntnu.no>
2014-01-10 14:57:00 +0100

    added calculaciton of max residual

--- main.cpp
+++ main.cpp
@@ -12,6 +12,54 @@ using namespace cl;
 #define KERNELS_DIR ""
 #endif
 
+float calculateMaxResidual(
+        SIPL::Volume<SIPL::float3>* vectorField,
+        SIPL::Volume<float> * v,
+        float mu
+    ) {
+    float maxResidual = -1;
+
+    // Calculate initial vector field first
+    SIPL::Volume<SIPL::float3> * initialVectorField = new SIPL::Volume<SIPL::float3>(v->getSize());
+    for(int x = 1; x < v->getWidth()-1; x++) {
+    for(int y = 1; y < v->getHeight()-1; y++) {
+    for(int z = 1; z < v->getDepth()-1; z++) {
+        SIPL::float3 vector;
+        vector.x = 0.5*(v->get(x+1,y,z)-v->get(x-1,y,z));
+        vector.y = 0.5*(v->get(x,y+1,z)-v->get(x,y-1,z));
+        vector.z = 0.5*(v->get(x,y,z+1)-v->get(x,y,z-1));
+        initialVectorField->set(x,y,z,vector);
+    }}}
+
+    // Go through each voxel and calculate the residual
+    for(int z = 1; z < vectorField->getDepth()-1; z++) {
+    for(int y = 1; y < vectorField->getHeight()-1; y++) {
+    for(int x = 1; x < vectorField->getWidth()-1; x++) {
+        SIPL::int3 pos(x,y,z);
+        SIPL::float3 v2 = vectorField->get(pos);
+        SIPL::float3 f = initialVectorField->get(pos);
+        SIPL::float3 laplacian = -6*v2
+                + vectorField->get(pos+SIPL::int3(1,0,0))
+                + vectorField->get(pos+SIPL::int3(0,1,0))
+                + vectorField->get(pos+SIPL::int3(0,0,1))
+                + vectorField->get(pos-SIPL::int3(1,0,0))
+                + vectorField->get(pos-SIPL::int3(0,1,0))
+                + vectorField->get(pos-SIPL::int3(0,0,1));
+        SIPL::float3 residual = mu * laplacian - (f.x*f.x+f.y*f.y+f.z*f.z)*(v2-f);
+        if(fabs(residual.x) > maxResidual) {
+            maxResidual = fabs(residual.x);
+        }
+        if(fabs(residual.y) > maxResidual) {
+            maxResidual = fabs(residual.y);
+        }
+        if(fabs(residual.z) > maxResidual) {
+            maxResidual = fabs(residual.z);
+        }
+    }}}
+
+    return maxResidual;
+}
+
 SIPL::float2 * run2DKernels(Context context, CommandQueue queue, float * voxels, int SIZE_X, int SIZE_Y, float mu, int ITERATIONS, int datatype) {
     ImageFormat storageFormat;
     if(datatype == sizeof(short)) {
@@ -161,7 +209,6 @@ SIPL::float3 * run3DKernels(Context context, CommandQueue queue, float * voxels,
 
     // Load volume to GPU
     Image3D volume = Image3D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, ImageFormat(CL_R, CL_FLOAT), SIZE_X, SIZE_Y, SIZE_Z, 0, 0, voxels);
-    delete[] voxels;
 
     // Run initialization kernel
     cl::size_t<3> offset;
@@ -289,7 +336,6 @@ SIPL::float3 * run3DKernelsWithoutTexture(Context context, CommandQueue queue, f
 
     // Load volume to GPU
     Image3D volume = Image3D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, ImageFormat(CL_R, CL_FLOAT), SIZE_X, SIZE_Y, SIZE_Z, 0, 0, voxels);
-    delete[] voxels;
 
     // Run initialization kernel
     cl::size_t<3> offset;
@@ -452,7 +498,9 @@ int main(int argc, char ** argv) {
             }
             GVF->setData(output);
             GVF->display(0, 0.1);
-        }else{
+
+            std::cout << "Max residual (error) was: " << calculateMaxResidual(GVF, volume, mu) << std::endl;
+        } else {
             // Is 2D image
             std::cout << "Reading image file " << filename << std::endl;
             // TODO: need to normalized this image!! Current code will not work
